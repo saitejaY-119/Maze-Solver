13c4ed69fecf1942a34b5dc4e974d4d8d8a9b28a
saitejaY-119 <137282727+saitejaY-119@users.noreply.github.com>
2024-02-18 15:20:51 +0530

    Add files via upload

---
+++ MazeSolver/src/main/java/com/mycompany/mazesolver/MazeSolver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mazesolver;
+
+/**
+ *
+ * @author Windows
+ */
+public class MazeSolver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
